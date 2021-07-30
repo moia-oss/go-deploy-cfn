4704229d48b4af04e984307e3ee168c0824ac924
cbrgm <chris@cbrgm.net>
2021-07-30 12:53:42 +0200

    linter happiness

--- cloudformation.go
+++ cloudformation.go
@@ -19,13 +19,13 @@ import (
 )
 
 // Cloudformation is a utility wrapper around the original aws api to make
-// common operations more intuitive
+// common operations more intuitive.
 type Cloudformation struct {
 	CFClient  cloudformationiface.CloudFormationAPI
 	StackName string
 }
 
-// CloudformationAPI provides an API which can be used instead of a concrete client for testing/mocking purposes
+// CloudformationAPI provides an API which can be used instead of a concrete client for testing/mocking purposes.
 type CloudformationAPI interface {
 	CloudFormationDeploy(templateBody string, namedIAM bool) error
 }
@@ -43,7 +43,7 @@ func (c *Cloudformation) getCreateType() (string, error) {
 
 	_, err := c.CFClient.DescribeStacks(dsi)
 	if err != nil && !strings.Contains(err.Error(), "does not exist") {
-		return "", fmt.Errorf("unexpected error while describing stack: %s", err)
+		return "", fmt.Errorf("unexpected error while describing stack: %w", err)
 	}
 
 	if err != nil {
@@ -74,7 +74,7 @@ func (c *Cloudformation) executeChangeSet(changeSetName string) error {
 
 	_, err := c.CFClient.ExecuteChangeSet(ecsi)
 	if err != nil {
-		return fmt.Errorf("error executing the ChangeSet: %s", err)
+		return fmt.Errorf("error executing the ChangeSet: %w", err)
 	}
 
 	return try.Do(func(attempt int) (bool, error) {
@@ -84,19 +84,24 @@ func (c *Cloudformation) executeChangeSet(changeSetName string) error {
 		}
 		dso, err := c.CFClient.DescribeStacks(dsi)
 		if err != nil {
-			return false, fmt.Errorf("error describing the stack: %s", err)
+			return false, fmt.Errorf("error describing the stack: %w", err)
 		}
 
 		if len(dso.Stacks) != 1 {
-			return false, fmt.Errorf("unexpected (!=1) number of stacks in result: %v", len(dso.Stacks))
+			err := fmt.Errorf("unexpected (!=1) number of stacks in result: %v", len(dso.Stacks))
+
+			return false, err
 		}
 
 		switch *dso.Stacks[0].StackStatus {
 		case cloudformation.StackStatusUpdateComplete, cloudformation.StackStatusCreateComplete, cloudformation.StackStatusUpdateCompleteCleanupInProgress:
 			log.Infof("ChangeSet '%s' has been successfully executed.", changeSetName)
+
 			return false, nil
 		case cloudformation.StackStatusCreateInProgress, cloudformation.StackStatusUpdateInProgress:
-			time.Sleep(6 * time.Second)
+			duration := time.Duration(6) * time.Second
+			time.Sleep(duration)
+
 			return attempt < 20, errors.New("stack not yet in completed state")
 		}
 
@@ -104,7 +109,7 @@ func (c *Cloudformation) executeChangeSet(changeSetName string) error {
 	})
 }
 
-// CloudFormationDeploy deploys the given Cloudformation Template to the given Cloudformation Stack
+// CloudFormationDeploy deploys the given Cloudformation Template to the given Cloudformation Stack.
 func (c *Cloudformation) CloudFormationDeploy(templateBody string, namedIAM bool) error {
 	changeSetType, err := c.getCreateType()
 	if err != nil {
@@ -113,7 +118,7 @@ func (c *Cloudformation) CloudFormationDeploy(templateBody string, namedIAM bool
 
 	id, err := uuid.NewUUID()
 	if err != nil {
-		return fmt.Errorf("error while generating UUID %s", err)
+		return fmt.Errorf("error while generating UUID %w", err)
 	}
 
 	// max stack name is 128, then we add a UUID (36 byte/char string) so the max the stackName can be is 92
@@ -136,7 +141,7 @@ func (c *Cloudformation) CloudFormationDeploy(templateBody string, namedIAM bool
 
 	ccso, err := c.CFClient.CreateChangeSet(ccsi)
 	if err != nil {
-		return fmt.Errorf("the ChangeSetType was %s error in creating ChangeSet: %s", changeSetType, err)
+		return fmt.Errorf("the ChangeSetType was %s error in creating ChangeSet: %w", changeSetType, err)
 	}
 
 	dcsi := &cloudformation.DescribeChangeSetInput{
@@ -144,31 +149,34 @@ func (c *Cloudformation) CloudFormationDeploy(templateBody string, namedIAM bool
 		StackName:     aws.String(sn),
 	}
 
+	maxAttempts := 12
+	delay := time.Duration(5) * time.Second
+
 	err = c.CFClient.WaitUntilChangeSetCreateCompleteWithContext(context.Background(),
 		dcsi,
-		request.WithWaiterDelay(request.ConstantWaiterDelay(5*time.Second)),
-		request.WithWaiterMaxAttempts(12))
+		request.WithWaiterDelay(request.ConstantWaiterDelay(delay)),
+		request.WithWaiterMaxAttempts(maxAttempts))
 
 	if err != nil {
 		dcso, err2 := c.CFClient.DescribeChangeSet(dcsi)
 
 		if err2 != nil {
-			return fmt.Errorf("error describing the ChangeSet: %s", err2)
+			return fmt.Errorf("error describing the ChangeSet: %w", err2)
 		}
 
 		if changeSetIsEmpty(dcso) {
 			log.Infof("ChangeSet '%v' is empty. Nothing to do.", *ccso.Id)
+
 			return nil
 		}
 
-		return fmt.Errorf("changeset is not empty but waiting for changeset completion still timed out. "+
-			"Error was: %s", err)
+		return fmt.Errorf("changeset is not empty but waiting for changeset completion still timed out. Error was: %w", err)
 	}
 
 	return c.executeChangeSet(csn)
 }
 
-// CreateStackName creates a valid stack name from the given alarm name
+// CreateStackName creates a valid stack name from the given alarm name.
 func CreateStackName(s string) string {
 	s = strings.ToLower(s)
 	for _, char := range [...]string{"/", "."} {
@@ -178,7 +186,7 @@ func CreateStackName(s string) string {
 	return s
 }
 
-// CreateLogicalName creates a logical name used in the CloudFormation template
+// CreateLogicalName creates a logical name used in the CloudFormation template.
 func CreateLogicalName(s string) string {
 	for _, char := range [...]string{"-", "/", "_", "."} {
 		s = strings.ReplaceAll(s, char, "")
